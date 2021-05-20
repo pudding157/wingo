3149785978e5e10455578e783cdf174d96f2b38d
puddingwin <chayasorn.want@gmail.com>
2021-05-20 13:38:44 +0700

    fix config

--- app/config.go
+++ app/config.go
@@ -27,27 +27,18 @@ func NewConfig(env string) *Config {
 
 //Init func
 func (c *Config) Init() error {
-	vp := viper.New()
-	vp.AddConfigPath("./config")
-	vp.SetConfigName(c.Env)
-	c.vp = vp
-	// c.Db = &Database{
-	// 	Merchants: []models.Merchant{{
-	// 		ID:           1,
-	// 		Name:         "test",
-	// 		BangkAccount: "1234",
-	// 		Username:     "abc1234",
-	// 		Password:     "1234",
-	// 	}},
-	// 	Products: []models.Product{},
-	// }
+	// vp := viper.New()
+	// vp.AddConfigPath("./config")
+	// vp.SetConfigName(c.Env)
+	// c.vp = vp
+
 	c.Db = &Database{}
-	if err := vp.ReadInConfig(); err != nil {
-		return err
-	}
-	if err := c.binding(); err != nil {
-		return err
-	}
+	// if err := vp.ReadInConfig(); err != nil {
+	// 	return err
+	// }
+	// if err := c.binding(); err != nil {
+	// 	return err
+	// }
 	return nil
 }
 
