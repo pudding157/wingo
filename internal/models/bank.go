ea38d43adf2de55787f7686bbf8199f6ddaea67a
puddingwin <chayasorn.want@gmail.com>
2021-06-18 10:32:23 +0700

    rename bankid to id

--- internal/models/bank.go
+++ internal/models/bank.go
@@ -16,7 +16,7 @@ type AdminBank struct {
 }
 
 type AdminBankModel struct {
-	BankId      int    `json:"bank_id"`
+	BankId      int    `json:"id"`
 	BankName    string `json:"name"`
 	BankAccount string `gorm:"unique;not null" json:"bank_account"`
 }
